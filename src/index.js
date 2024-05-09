f22486ce292f0353a69e0df7bb9b19f2b4db0e26
Ty Gagielo <ty.gagielo@gmail.com>
2024-05-09 20:43:59 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import { Container } from 'reactstrap';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Container>
+      <App />
+    </Container>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
